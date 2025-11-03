58cf563e84380578ec2eae78e88067da4e9906a8
inter <53667669+aijiesd520@users.noreply.github.com>
2025-11-03 09:43:05 +0800

    Update new_v8.sh

--- new_v8.sh
+++ new_v8.sh
@@ -31,7 +31,7 @@ echo "[+] 检测到公网 IP 共 ${#IPS[@]} 个"
 
 # 随机密码函数
 gen_pwd() {
-    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 8 || true
+    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 8 | tr 'A-Z' 'a-z' || true
 }
 
 # 生成用户、密码与配置
@@ -62,7 +62,6 @@ echo "[+] passwd 文件： $PASSWD_FILE"
 echo "[+] outgoing 配置： $OUTGOING_CONF"
 echo "[+] 明文用户列表（请妥善保管）： $USER_LIST_FILE"
 
-
 echo "下一步（必须）:"
 echo "1) 检查 $SQUID_CONF 中是否包含："
 echo "   include /etc/squid/outgoing_map.conf"
@@ -80,21 +79,17 @@ cache_dir ufs /var/spool/squid 10000 16 256
 
 access_log none
 cache_log /var/log/squid/cache.log
+acl SSL_ports port 443
+acl Safe_ports port 80 21 443 70 210 51128 280 488 591 777
+acl CONNECT method CONNECT
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
 auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
 auth_param basic realm Proxy
 acl authenticated proxy_auth REQUIRED
-
-acl SSL_ports port 443
-acl Safe_ports port 80 21 443 70 210 1025-65535 280 488 591 777
-acl CONNECT method CONNECT
-
-# 先允许认证，再限制端口
 http_access allow authenticated
-http_access deny !Safe_ports
-http_access deny CONNECT !SSL_ports
 http_access deny all
-
-visible_hostname PI10P66
+visible_hostname PI10P65
 http_port 0.0.0.0:51128
 include /etc/squid/outgoing_map.conf
 EOF
